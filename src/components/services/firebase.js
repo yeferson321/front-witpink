035961147073737747b181cb731bb8a267a4195b
Yeferson <131elyefer@gmail.com>
2022-11-25 12:01:45 -0500

    nueva

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 For Firebase JS SDK v7.20.0 and later, measurementId is optional
 Importing the firebaseConfig from the .env file. */
 const firebaseConfig = {
-  apiKey: "AIzaSyD03fzy7mj7CA8rB0h1yN0XDLfyX3vTA7Q",
-  authDomain: "apideployusers.firebaseapp.com",
-  projectId: "apideployusers",
-  storageBucket: "apideployusers.appspot.com",
-  messagingSenderId: "609618507337",
-  appId: "1:609618507337:web:900baaea5330e835d6b198",
-  measurementId: "G-R5HSXYFQSG"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 /* Exporting the app and auth from the firebaseConfig. */
